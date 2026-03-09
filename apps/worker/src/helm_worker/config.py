b13cf17f7fa86524c6d334e54cfc0a41509e9f17
Ankush Patel <patelankush.2012@gmail.com>
2026-03-09 01:09:16 -0700

    feat: centralize shared runtime config

--- apps/worker/src/helm_worker/config.py
+++ apps/worker/src/helm_worker/config.py
@@ -1,8 +1,8 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from helm_runtime import RuntimeAppSettings, runtime_settings_config
 
 
-class WorkerSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+class WorkerSettings(RuntimeAppSettings):
+    model_config = runtime_settings_config()
 
     worker_poll_seconds: int = 30
 
