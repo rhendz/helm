43631c70c47be8b11ae94ae938c60b630cb01c22
Ankush Patel <patelankush.2012@gmail.com>
2026-03-10 02:35:06 -0700

    feat: add scheduled thread task execution scaffold

--- tests/unit/test_worker_registry.py
+++ tests/unit/test_worker_registry.py
@@ -2,4 +2,4 @@ from helm_worker.jobs.registry import JOBS
 
 
 def test_job_registry_contains_core_jobs() -> None:
-    assert {"email_triage", "digest", "study"}.issubset(JOBS.keys())
+    assert {"email_triage", "digest", "study", "scheduled_thread_tasks"}.issubset(JOBS.keys())
